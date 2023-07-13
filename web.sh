9fc7f98660e06379b3c3c5c9305e05cb006b0f63
Alexandre Strube <surak@surak.eti.br>
2023-07-13 18:02:11 +0000

    Add openai empty keys to web server

--- web.sh
+++ web.sh
@@ -5,6 +5,9 @@ source sc_venv_template/activate.sh
 
 #python3 -m fastchat.serve.gradio_web_server_multi --share
 #python3 -m fastchat.serve.gradio_web_server --model-list-mode=reload --host 0.0.0.0
+export OPENAI_API_KEY=EMPTY
+export OPENAI_API_BASE=http://localhost:8000/v1
+
 python3 fastchat/serve/gradio_web_server_branded.py \
         --share \
         --model-list-mode=reload \
