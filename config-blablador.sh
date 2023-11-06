a38f933a5241fd9945d8c8eca557057ace5f8bb8
Alexandre Strube <surak@surak.eti.br>
2023-11-06 17:54:52 +0000

    blablador config points to vm

--- config-blablador.sh
+++ config-blablador.sh
@@ -3,6 +3,6 @@ export LOGDIR=$BLABLADOR_DIR/logs
 export NCCL_P2P_DISABLE=1 # 3090s do not support p2p
 #export BLABLADOR_CONTROLLER=http://haicluster1.fz-juelich.de
 # WHhile haicluster1 is down
-export BLABLADOR_CONTROLLER=http://haicluster2.fz-juelich.de
+export BLABLADOR_CONTROLLER=http://134.94.199.27
 export BLABLADOR_CONTROLLER_PORT=21001
 
