db7f12c04dc3d2de1eaa3a7a97af28ed91d66939
Alexandre Strube <surak@surak.eti.br>
2025-01-14 17:58:50 +0000

    branded

--- web.sh
+++ web.sh
@@ -3,10 +3,10 @@
 cd $HOME/Devel/FastChat
 source sc_venv_template/activate.sh
 
-python3 fastchat/serve/gradio_web_server_multi.py \
+python3 fastchat/serve/gradio_web_server_branded.py \
         --model-list-mode=reload \
         --host 0.0.0.0 \
         --port 7860 \
         --controller-url http://helmholtz-blablador.fz-juelich.de:21001 \
-        --vision-arena
+#        --vision-arena
 #        --register $HOME/Devel/FastChat/ollama_api_endpoints.json
